5950c9b4669b58e565de6479f2127db9f3ff7a31
louislj11 <louisjaheeim@web.de>
2025-07-05 19:28:43 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+body {
+  background-color: #222;
+  color: white;
+  font-family: monospace;
+  text-align: center;
+  margin: 0;
+  padding: 20px;
+}
+
+canvas {
+  background-color: #333;
+  border: 2px solid white;
+}
